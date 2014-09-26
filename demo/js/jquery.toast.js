82e5bd663f88abaf445718488fcda481550866fa
Kamran Ahmed <kamranahmed.se@gmail.com>
2014-09-26 12:42:09 +0500

    Allow the user to provide the stackLength

--- demo/js/jquery.toast.js
+++ demo/js/jquery.toast.js
@@ -61,20 +61,22 @@ if ( typeof Object.create !== 'function' ) {
 			 if ( _container.length === 0 ) {
 			 	_container = $('<div class="jq-toast-wrap"></div>');
 			 	$('body').append( _container );
+			 } else if ( !this.options.stackLength || isNaN( parseInt(this.options.stackLength, 10) ) ) {
+			 	_container.empty();
 			 }
 
 			 _container.append( this.options.toastEl );
 
-		  	if ( this.options.maxToasts && !isNaN( parseInt( this.options.maxToasts ), 10 ) ) {
+		  	if ( this.options.stackLength && !isNaN( parseInt( this.options.stackLength ), 10 ) ) {
 		 	 	
 		 	 	var _prevToastCount = _container.find('.jq-toast-single').length,
-		 	 		_extToastCount = _prevToastCount - this.options.maxToasts;
+		 	 		_extToastCount = _prevToastCount - this.options.stackLength;
 
 		 	 	if ( _extToastCount > 0 ) {
 		  			$('.jq-toast-wrap').find('.jq-toast-single').slice(0, _extToastCount).remove();
 		 	 	};
 
-		  	};
+		  	}
 		},
 		animate: function () {
 			this.options.toastEl.hide();
@@ -124,7 +126,7 @@ if ( typeof Object.create !== 'function' ) {
 		heading: '',
 		showHideTransition: 'fade',
 		hideAfter: false,
-		maxToasts: 5
+		stackLength: 5
 	};
 
 })( jQuery, window, document );
