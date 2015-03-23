14468b0a1d2a5d8e57c2601be3a4773a2038f86c
Kamran Ahmed <kamranahmed.se@gmail.com>
2015-03-23 13:40:09 +0500

    Updated the js and css for the dist as per the newer functionality

--- jquery.toast.js
+++ jquery.toast.js
@@ -2,308 +2,315 @@
 // jQuery toast plugin created by Kamran Ahmed copyright MIT license 2014
 // v0.1
 if ( typeof Object.create !== 'function' ) {
-	Object.create = function( obj ) {
-		function F() {}
-		F.prototype = obj;
-		return new F();
-	};
+    Object.create = function( obj ) {
+        function F() {}
+        F.prototype = obj;
+        return new F();
+    };
 }
 (function( $, window, document, undefined ) {
 
-	"use strict";
-	
-	var Toast = {
+    "use strict";
+    
+    var Toast = {
 
-		_positionClasses : ['bottom-left', 'bottom-right', 'top-right', 'top-left', 'bottom-center', 'top-center', 'mid-center'],
-		_defaultIcons : ['success', 'error', 'info', 'warning'],
+        _positionClasses : ['bottom-left', 'bottom-right', 'top-right', 'top-left', 'bottom-center', 'top-center', 'mid-center'],
+        _defaultIcons : ['success', 'error', 'info', 'warning'],
 
-		init: function (options, elem) {
-			this.prepareOptions(options, $.toast.options);
-			this.process();
-		},
+        init: function (options, elem) {
+            this.prepareOptions(options, $.toast.options);
+            this.process();
+        },
 
-		prepareOptions: function(options, options_to_extend) {
-			var _options = {};
-			if ( ( typeof options === 'string' ) || ( options instanceof Array ) ) {
-				_options.text = options;
-			} else {
-				_options = options;
-			}
-			this.options = $.extend( {}, options_to_extend, _options );
-		},
+        prepareOptions: function(options, options_to_extend) {
+            var _options = {};
+            if ( ( typeof options === 'string' ) || ( options instanceof Array ) ) {
+                _options.text = options;
+            } else {
+                _options = options;
+            }
+            this.options = $.extend( {}, options_to_extend, _options );
+        },
 
-		process: function () {
-			this.setup();
-			this.addToDom();
-			this.position();
-			this.bindToast();
-			this.animate();
-		},
+        process: function () {
+            this.setup();
+            this.addToDom();
+            this.position();
+            this.bindToast();
+            this.animate();
+        },
 
-		setup: function () {
-			
-			var _toastContent = '';
-			
-			this._toastEl = this._toastEl || $('<div></div>', {
-				class : 'jq-toast-single'
-			});
+        setup: function () {
+            
+            var _toastContent = '';
+            
+            this._toastEl = this._toastEl || $('<div></div>', {
+                class : 'jq-toast-single'
+            });
 
-			if ( this.options.allowToastClose ) {
-				_toastContent += '<span class="close-jq-toast-single">&times;</span>';
-			};
+            if ( this.options.allowToastClose ) {
+                _toastContent += '<span class="close-jq-toast-single">&times;</span>';
+            };
 
-			if ( this.options.text instanceof Array ) {
+            if ( this.options.text instanceof Array ) {
 
-				if ( this.options.heading ) {
-					_toastContent +='<h2 class="jq-toast-heading">' + this.options.heading + '</h2>';
-				};
+                if ( this.options.heading ) {
+                    _toastContent +='<h2 class="jq-toast-heading">' + this.options.heading + '</h2>';
+                };
 
-				_toastContent += '<ul class="jq-toast-ul">';
-				for (var i = 0; i < this.options.text.length; i++) {
-					_toastContent += '<li class="jq-toast-li" id="jq-toast-item-' + i + '">' + this.options.text[i] + '</li>';
-				}
-				_toastContent += '</ul>';
+                _toastContent += '<ul class="jq-toast-ul">';
+                for (var i = 0; i < this.options.text.length; i++) {
+                    _toastContent += '<li class="jq-toast-li" id="jq-toast-item-' + i + '">' + this.options.text[i] + '</li>';
+                }
+                _toastContent += '</ul>';
 
-			} else {
-				if ( this.options.heading ) {
-					_toastContent +='<h2 class="jq-toast-heading">' + this.options.heading + '</h2>';
-				};
-				_toastContent += this.options.text;
-			}
+            } else {
+                if ( this.options.heading ) {
+                    _toastContent +='<h2 class="jq-toast-heading">' + this.options.heading + '</h2>';
+                };
+                _toastContent += this.options.text;
+            }
 
-			this._toastEl.html( _toastContent );
+            this._toastEl.html( _toastContent );
 
-			if ( this.options.bgColor ) {
-				this._toastEl.css("background-color", this.options.bgColor);
-				this._toastEl.css("color", this.options.textColor);
-			};
+            if ( this.options.bgColor !== false ) {
+                this._toastEl.css("background-color", this.options.bgColor);
+            };
 
-			if ( this.options.textAlign ) {
-				this._toastEl.css('text-align', this.options.textAlign);
-			}
+            if ( this.options.textColor !== false ) {
+                this._toastEl.css("color", this.options.textColor);
+            };
 
-			if ( this.options.icon !== false ) {
-				this._toastEl.addClass('jq-has-icon');
-			};
-		},
+            if ( this.options.textAlign ) {
+                this._toastEl.css('text-align', this.options.textAlign);
+            }
 
-		position: function () {
-			if ( ( typeof this.options.position === 'string' ) && ( $.inArray( this.options.position, this._positionClasses) !== -1 ) ) {
+            if ( this.options.icon !== false ) {
+                this._toastEl.addClass('jq-has-icon');
 
-				if ( this.options.position === 'bottom-center' ) {
-					this._container.css({
-					    left: ( $(window).outerWidth() / 2 ) - this._container.outerWidth()/2,
-					    bottom: 20
-					});
-				} else if ( this.options.position === 'top-center' ) {
-					this._container.css({
-					    left: ( $(window).outerWidth() / 2 ) - this._container.outerWidth()/2,
-					    top: 20
-					});
-				} else if ( this.options.position === 'mid-center' ) {
-					this._container.css({
-					    left: ( $(window).outerWidth() / 2 ) - this._container.outerWidth()/2,
-					    top: ( $(window).outerHeight() / 2 ) - this._container.outerHeight()/2
-					});
-				} else {
-					this._container.addClass( this.options.position );
-				}
+                if ( $.inArray(this.options.icon, this._defaultIcons) !== -1 ) {
+                    this._toastEl.addClass('jq-icon-' + this.options.icon);
+                };
+            };
+        },
 
-			} else if ( typeof this.options.position === 'object' ) {
-				this._container.css({
-					top : this.options.position.top ? this.options.position.top : 'auto',
-					bottom : this.options.position.bottom ? this.options.position.bottom : 'auto',
-					left : this.options.position.left ? this.options.position.left : 'auto',
-					right : this.options.position.right ? this.options.position.right : 'auto'
-				});
-			} else {
-				this._container.addClass( 'bottom-left' );
-			}
-		},
+        position: function () {
+            if ( ( typeof this.options.position === 'string' ) && ( $.inArray( this.options.position, this._positionClasses) !== -1 ) ) {
 
-		bindToast: function () {
+                if ( this.options.position === 'bottom-center' ) {
+                    this._container.css({
+                        left: ( $(window).outerWidth() / 2 ) - this._container.outerWidth()/2,
+                        bottom: 20
+                    });
+                } else if ( this.options.position === 'top-center' ) {
+                    this._container.css({
+                        left: ( $(window).outerWidth() / 2 ) - this._container.outerWidth()/2,
+                        top: 20
+                    });
+                } else if ( this.options.position === 'mid-center' ) {
+                    this._container.css({
+                        left: ( $(window).outerWidth() / 2 ) - this._container.outerWidth()/2,
+                        top: ( $(window).outerHeight() / 2 ) - this._container.outerHeight()/2
+                    });
+                } else {
+                    this._container.addClass( this.options.position );
+                }
 
-			var that = this;
+            } else if ( typeof this.options.position === 'object' ) {
+                this._container.css({
+                    top : this.options.position.top ? this.options.position.top : 'auto',
+                    bottom : this.options.position.bottom ? this.options.position.bottom : 'auto',
+                    left : this.options.position.left ? this.options.position.left : 'auto',
+                    right : this.options.position.right ? this.options.position.right : 'auto'
+                });
+            } else {
+                this._container.addClass( 'bottom-left' );
+            }
+        },
 
-			this._toastEl.find('.close-jq-toast-single').on('click', function ( e ) {
+        bindToast: function () {
 
-				e.preventDefault();
+            var that = this;
 
-				if( that.options.showHideTransition === 'fade') {
-					that._toastEl.trigger('beforeHide');
-					that._toastEl.fadeOut(function () {
-						that._toastEl.trigger('afterHidden');
-					});
-				} else if ( that.options.showHideTransition === 'slide' ) {
-					that._toastEl.trigger('beforeHide');
-					that._toastEl.slideUp(function () {
-						that._toastEl.trigger('afterHidden');
-					});
-				} else {
-					that._toastEl.trigger('beforeHide');
-					that._toastEl.hide(function () {
-						that._toastEl.trigger('afterHidden');
-					});
-				}
-			});
+            this._toastEl.find('.close-jq-toast-single').on('click', function ( e ) {
 
-			if ( typeof this.options.beforeShow == 'function' ) {
-				this._toastEl.on('beforeShow', function () {
-					that.options.beforeShow();
-				});
-			};
+                e.preventDefault();
 
-			if ( typeof this.options.afterShown == 'function' ) {
-				this._toastEl.on('afterShown', function () {
-					that.options.afterShown();
-				});
-			};
+                if( that.options.showHideTransition === 'fade') {
+                    that._toastEl.trigger('beforeHide');
+                    that._toastEl.fadeOut(function () {
+                        that._toastEl.trigger('afterHidden');
+                    });
+                } else if ( that.options.showHideTransition === 'slide' ) {
+                    that._toastEl.trigger('beforeHide');
+                    that._toastEl.slideUp(function () {
+                        that._toastEl.trigger('afterHidden');
+                    });
+                } else {
+                    that._toastEl.trigger('beforeHide');
+                    that._toastEl.hide(function () {
+                        that._toastEl.trigger('afterHidden');
+                    });
+                }
+            });
 
-			if ( typeof this.options.beforeHide == 'function' ) {
-				this._toastEl.on('beforeHide', function () {
-					that.options.beforeHide();
-				});
-			};
+            if ( typeof this.options.beforeShow == 'function' ) {
+                this._toastEl.on('beforeShow', function () {
+                    that.options.beforeShow();
+                });
+            };
 
-			if ( typeof this.options.afterHidden == 'function' ) {
-				this._toastEl.on('afterHidden', function () {
-					that.options.afterHidden();
-				});
-			};			
-		},
+            if ( typeof this.options.afterShown == 'function' ) {
+                this._toastEl.on('afterShown', function () {
+                    that.options.afterShown();
+                });
+            };
 
-		addToDom: function () {
+            if ( typeof this.options.beforeHide == 'function' ) {
+                this._toastEl.on('beforeHide', function () {
+                    that.options.beforeHide();
+                });
+            };
 
-			 var _container = $('.jq-toast-wrap');
-			 
-			 if ( _container.length === 0 ) {
-			 	
-			 	_container = $('<div></div>',{
-			 		class: "jq-toast-wrap"
-			 	});
+            if ( typeof this.options.afterHidden == 'function' ) {
+                this._toastEl.on('afterHidden', function () {
+                    that.options.afterHidden();
+                });
+            };          
+        },
 
-			 	$('body').append( _container );
+        addToDom: function () {
 
-			 } else if ( !this.options.stack || isNaN( parseInt(this.options.stack, 10) ) ) {
-			 	_container.empty();
-			 }
+             var _container = $('.jq-toast-wrap');
+             
+             if ( _container.length === 0 ) {
+                
+                _container = $('<div></div>',{
+                    class: "jq-toast-wrap"
+                });
 
-			 _container.find('.jq-toast-single:hidden').remove();
+                $('body').append( _container );
 
-			 _container.append( this._toastEl );
+             } else if ( !this.options.stack || isNaN( parseInt(this.options.stack, 10) ) ) {
+                _container.empty();
+             }
 
-		  	if ( this.options.stack && !isNaN( parseInt( this.options.stack ), 10 ) ) {
-		 	 	
-		 	 	var _prevToastCount = _container.find('.jq-toast-single').length,
-		 	 		_extToastCount = _prevToastCount - this.options.stack;
+             _container.find('.jq-toast-single:hidden').remove();
 
-		 	 	if ( _extToastCount > 0 ) {
-		  			$('.jq-toast-wrap').find('.jq-toast-single').slice(0, _extToastCount).remove();
-		 	 	};
+             _container.append( this._toastEl );
 
-		  	}
+            if ( this.options.stack && !isNaN( parseInt( this.options.stack ), 10 ) ) {
+                
+                var _prevToastCount = _container.find('.jq-toast-single').length,
+                    _extToastCount = _prevToastCount - this.options.stack;
 
-		  	this._container = _container;
-		},
+                if ( _extToastCount > 0 ) {
+                    $('.jq-toast-wrap').find('.jq-toast-single').slice(0, _extToastCount).remove();
+                };
 
-		animate: function () {
+            }
 
-			var that = this;
+            this._container = _container;
+        },
 
-			this._toastEl.hide();
+        animate: function () {
 
-			this._toastEl.trigger('beforeShow');
+            var that = this;
 
-			if ( this.options.showHideTransition.toLowerCase() === 'fade' ) {
-				this._toastEl.fadeIn(function ( ){
-					that._toastEl.trigger('afterShown');
-				});
-			} else if ( this.options.showHideTransition.toLowerCase() === 'slide' ) {
-				this._toastEl.slideDown(function ( ){
-					that._toastEl.trigger('afterShown');
-				});
-			} else {
-				this._toastEl.show(function ( ){
-					that._toastEl.trigger('afterShown');
-				});
-			}
+            this._toastEl.hide();
 
-			if ( ( this.options.hideAfter !== false ) && !isNaN( parseInt( this.options.hideAfter, 10 ) ) ) {
+            this._toastEl.trigger('beforeShow');
 
-				var that = this;
+            if ( this.options.showHideTransition.toLowerCase() === 'fade' ) {
+                this._toastEl.fadeIn(function ( ){
+                    that._toastEl.trigger('afterShown');
+                });
+            } else if ( this.options.showHideTransition.toLowerCase() === 'slide' ) {
+                this._toastEl.slideDown(function ( ){
+                    that._toastEl.trigger('afterShown');
+                });
+            } else {
+                this._toastEl.show(function ( ){
+                    that._toastEl.trigger('afterShown');
+                });
+            }
 
-				window.setTimeout(function(){
-					
-					if ( that.options.showHideTransition.toLowerCase() === 'fade' ) {
-						that._toastEl.trigger('beforeHide');
-						that._toastEl.fadeOut(function () {
-							that._toastEl.trigger('afterHidden');
-						});
-					} else if ( that.options.showHideTransition.toLowerCase() === 'slide' ) {
-						that._toastEl.trigger('beforeHide');
-						that._toastEl.slideUp(function () {
-							that._toastEl.trigger('afterHidden');
-						});
-					} else {
-						that._toastEl.trigger('beforeHide');
-						that._toastEl.hide(function () {
-							that._toastEl.trigger('afterHidden');
-						});
-					}
+            if ( ( this.options.hideAfter !== false ) && !isNaN( parseInt( this.options.hideAfter, 10 ) ) ) {
 
-				}, this.options.hideAfter);
-			};
-		},
+                var that = this;
 
-		reset: function ( resetWhat ) {
+                window.setTimeout(function(){
+                    
+                    if ( that.options.showHideTransition.toLowerCase() === 'fade' ) {
+                        that._toastEl.trigger('beforeHide');
+                        that._toastEl.fadeOut(function () {
+                            that._toastEl.trigger('afterHidden');
+                        });
+                    } else if ( that.options.showHideTransition.toLowerCase() === 'slide' ) {
+                        that._toastEl.trigger('beforeHide');
+                        that._toastEl.slideUp(function () {
+                            that._toastEl.trigger('afterHidden');
+                        });
+                    } else {
+                        that._toastEl.trigger('beforeHide');
+                        that._toastEl.hide(function () {
+                            that._toastEl.trigger('afterHidden');
+                        });
+                    }
 
-			if ( resetWhat === 'all' ) {
-				$('.jq-toast-wrap').remove();
-			} else {
-				this._toastEl.remove();
-			}
+                }, this.options.hideAfter);
+            };
+        },
 
-		},
+        reset: function ( resetWhat ) {
 
-		update: function(options) {
-			this.prepareOptions(options, this.options);
-			this.setup();
-			this.bindToast();
-		}
-	};
-	
-	$.toast = function(options) {
-		var toast = Object.create(Toast);
-		toast.init(options, this);
+            if ( resetWhat === 'all' ) {
+                $('.jq-toast-wrap').remove();
+            } else {
+                this._toastEl.remove();
+            }
 
-		return {
-			
-			reset: function ( what ) {
-				toast.reset( what );
-			},
+        },
 
-			update: function( options ) {
-				toast.update( options );
-			}
-		}
-	};
+        update: function(options) {
+            this.prepareOptions(options, this.options);
+            this.setup();
+            this.bindToast();
+        }
+    };
+    
+    $.toast = function(options) {
+        var toast = Object.create(Toast);
+        toast.init(options, this);
 
-	$.toast.options = {
-		text: '',
-		heading: '',
-		showHideTransition: 'fade',
-		allowToastClose: true,
-		hideAfter: 5000,
-		stack: 5,
-		icon: false,
-		position: 'bottom-left',
-		bgColor: '#444',
-		textColor: '#eee',
-		textAlign: 'left',
-		beforeShow: function () {},
-		afterShown: function () {},
-		beforeHide: function () {},
-		afterHidden: function () {}
-	};
+        return {
+            
+            reset: function ( what ) {
+                toast.reset( what );
+            },
+
+            update: function( options ) {
+                toast.update( options );
+            }
+        }
+    };
+
+    $.toast.options = {
+        text: '',
+        heading: '',
+        showHideTransition: 'fade',
+        allowToastClose: true,
+        hideAfter: 5000,
+        stack: 5,
+        position: 'bottom-left',
+        bgColor: false,
+        textColor: false,
+        textAlign: 'left',
+        icon: false,
+        beforeShow: function () {},
+        afterShown: function () {},
+        beforeHide: function () {},
+        afterHidden: function () {}
+    };
 
 })( jQuery, window, document );
